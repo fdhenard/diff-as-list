a9ddc174084884e8ecdd3556bd56ad3a0a747f44
Frank Henard <fdhenard@gmail.com>
2015-06-08 11:41:08 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to diff-as-list
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/diff_as_list/core_test.clj
@@ -0,0 +1,7 @@
+(ns diff-as-list.core-test
+  (:require [clojure.test :refer :all]
+            [diff-as-list.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
