da1c093eac1548a39b7b293d65136092251b6be9
Frank Henard <fdhenard@gmail.com>
2015-06-08 12:36:29 -0500

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject diff-as-list "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject diff-as-list "1.0.0"
+  :description "Compare clojure data structures. The differences come back as a list with paths to where the elements differ"
+  :url "https://github.com/fdhenard/diff-as-list"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
