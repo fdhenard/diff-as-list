1a666bc5adb1bd7942efd4ac72793ecc2a057310
Frank Henard <frank@mirusresearch.com>
2018-05-19 09:32:10 -0500

    upgrade clojure; start attempt 2

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/fdhenard/diff-as-list"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :scm {:name "git" :url "https://github.com/fdhenard/diff-as-list"})
