62113813afe84ecb39103d0793410d0f74d725dd
Frank Henard <frank@mirusresearch.com>
2018-05-23 07:03:29 -0500

    rename args

--- src/diff_as_list/attempt_2.clj
+++ src/diff_as_list/attempt_2.clj
@@ -112,13 +112,13 @@
                                    {:path _key :val-1 value-1 :val-2 value-2})))
         value-diffs (remove nil? (map #(keys-in-both-compare %) keys-in-both))
         value-diff-paths (map :path value-diffs)
-        is-path-child-of-other-path? (fn [path-2 path-1]
-                                       (let [path-1-length (count path-1)
-                                             path-2-length (count path-2)]
-                                         (if (< path-2-length path-1-length)
+        is-path-child-of-other-path? (fn [path-1 path-2]
+                                       (let [path-2-length (count path-2)
+                                             path-1-length (count path-1)]
+                                         (if (< path-1-length path-2-length)
                                            false
-                                           (let [path-2-shortened (take path-1-length path-2)]
-                                             (= path-1 path-2-shortened)))))
+                                           (let [path-1-shortened (take path-2-length path-1)]
+                                             (= path-2 path-1-shortened)))))
         ;; is-missing-key-in-diffs? #(contains? value-diff-path-set %)
         is-missing-path-in-diffs? (fn [missing-path]
                                     (some #(is-path-child-of-other-path? missing-path %) value-diff-paths))
