95a290aeefc9bc84c48a4620d663bb70dac1d2ff
Frank Henard <frank@mirusresearch.com>
2018-05-24 06:49:07 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diff-as-list "1.0.1"
+(defproject diff-as-list "2.0.0"
   :description "Compare clojure data structures. The differences come back as a list with paths to where the elements differ"
   :url "https://github.com/fdhenard/diff-as-list"
   :license {:name "Eclipse Public License"
